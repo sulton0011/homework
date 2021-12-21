deea16482504d57324580811501818bccf994201
sulton0011 <azizxonkom@gmail.com>
2021-12-22 01:04:59 +0500

    Check The Characters 0.1

---
+++ ikktadan_kop_harflar.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+func main () {
+	fmt.Println(("assbgbd"))
+}
+
+func CheckTheCharacters(s string) (n string){
+	for i := range s {
+		if strings.Count(s, string(s[i])) == 1{
+			n += string(s[i])
+		}
+	}
+
+	return 
+}
