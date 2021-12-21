e1f5fa922b21b57416d4137acee12d41207e53c1
sulton0011 <azizxonkom@gmail.com>
2021-12-22 00:50:07 +0500

    Password verifiation 0.3

--- password.go
+++ password.go
@@ -1,16 +1,22 @@
 package main
+
 import (
 	"fmt"
+	"unicode/utf8"
 )
 func main() {
-	fmt.Println(PasswordVerification("asdfads"))
+	fmt.Println(PasswordVerification("asdfampлs"))
 }
 
 func PasswordVerification(s string) string{
 	var PassVer bool
 
-	
-	
+	for i := range s {
+		if s[i] <= 90 && s[i] >= 65 || s[i] >= 97 && s[i] <= 122 {
+			fmt.Println("a")
+		}
+	}
+	fmt.Println(len(s), utf8.RuneCountInString(s))
 	if ! PassVer {		
 		return "Ok"
 	}else {
